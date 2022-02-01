3263f658f8eaefc9ca5fc402d52b3fce174d6d63
HAjulien <hajulien1@gmail.com>
2022-02-01 14:29:33 +0100

    entre

---
+++ js/main.js
@@ -0,0 +1,30 @@
+ let demo =  new Vue ({
+    el: '#demo',
+    data: {
+        show : true
+    }
+});
+
+
+let app1 = new Vue ({
+    el: '#app-1',
+    data:{
+        message:'Hello Vue.js'
+    }
+});
+
+let app2 = new Vue ({
+    el: '#app-2',
+    data:{
+        message:'Hello Vue.js'
+    }
+});
+
+let app3 = new Vue ({
+    el: '#app-3',
+    data:{
+        seen: true
+    }
+});
+
+
