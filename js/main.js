da4c9aeb9cc5e4d313001f1ff435740a111df1ca
HAjulien <96483338+HAjulien@users.noreply.github.com>
2022-02-01 14:43:00 +0100

    update

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
     data: {
         show : true
     }
-});
+})
 
 
 let app1 = new Vue ({
@@ -11,20 +11,27 @@ let app1 = new Vue ({
     data:{
         message:'Hello Vue.js'
     }
-});
+})
 
 let app2 = new Vue ({
     el: '#app-2',
     data:{
         message:'Hello Vue.js'
     }
-});
+})
 
 let app3 = new Vue ({
     el: '#app-3',
     data:{
         seen: true
     }
-});
+})
+
+const stock = new Vue({
+    el: '#app',
+    data:{
+        product:'New Balance'
+    }
+})
 
 
