66e41bcdb4f7b16826efa0b37cb829052af19cf1
HAjulien <hajulien1@gmail.com>
2022-02-08 16:37:35 +0100

    1

---
+++ project-vue3/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    css: {
+      loaderOptions: {
+        scss: {
+          additionalData: `
+            @import "@/scss/_variables.scss";
+            @import "@/scss/_mixins.scss";
+            @import "@/scss/_placeholders.scss";
+
+          `
+        }
+      }
+    }
+  };
